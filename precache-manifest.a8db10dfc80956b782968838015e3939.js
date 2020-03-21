d4330741d9bdd92d8225ea976c4bc1dfec760909
gianlopes <gianlucas1020@hotmail.com>
2020-03-21 01:32:12 -0300

    Updates

---
+++ precache-manifest.a8db10dfc80956b782968838015e3939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924c72ef7104ef755f31bf99cc77e954",
+    "url": "/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "1348182e0643f2912e9f",
+    "url": "/facerecognitionapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43726c0ce1cfe55f8859",
+    "url": "/facerecognitionapp/static/js/2.87359041.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionapp/static/js/2.87359041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1348182e0643f2912e9f",
+    "url": "/facerecognitionapp/static/js/main.84a68c6d.chunk.js"
+  },
+  {
+    "revision": "4f82fd8c48dbd28fc75a",
+    "url": "/facerecognitionapp/static/js/runtime-main.1e595546.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facerecognitionapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
